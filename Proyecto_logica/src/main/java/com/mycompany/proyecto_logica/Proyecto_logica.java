cb72f976178516b41829ea75cf6b9f0e0fd0b533
Mariana <tiradomariana66@gmail.com>
2026-04-18 11:28:08 -0500

    Proyecto para logica II

---
+++ Proyecto_logica/src/main/java/com/mycompany/proyecto_logica/Proyecto_logica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_logica;
+
+/**
+ *
+ * @author Mariana
+ */
+public class Proyecto_logica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
