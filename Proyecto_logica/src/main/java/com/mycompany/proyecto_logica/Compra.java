f34ba5119d9fc20c2e3695b3c70218bf43f68c83
Mariana8TiCa <tiradomariana66@gmail.com>
2026-04-24 14:38:01 -0500

    Cambio a interfaz de usuario

--- Proyecto_logica/src/main/java/com/mycompany/proyecto_logica/Compra.java
+++ Proyecto_logica/src/main/java/com/mycompany/proyecto_logica/Compra.java
@@ -9,79 +9,138 @@ package com.mycompany.proyecto_logica;
  * @author Mariana.
  */
 
+import javax.swing.*;
+import java.awt.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
-public class Compra {
-    public static void main(String args[]){
-        Scanner sc = new Scanner (System.in);
-        
-        HashMap <String, Cliente> clientes = new HashMap<>();
-        ArrayList <Mueble> productos = new ArrayList<>();
-        
-        int opcion = 0, opcion1 = 0;
-        
-        while(opcion != 10){
-            System.out.println("Ingrese una opcion:");
-            System.out.println("1. Registrar un cliente.");
-            System.out.println("2. Registrar un producto de tipo mesa o de tipo silla.");
-            System.out.println("3. Registrar una compra.");
-            System.out.println("4. Mostrar la cantidad de productos que ofrece la tienda y "
-                    + "la información de cada uno de estos productos.");
-            System.out.println("5. Calcular y mostrar el porcentaje de productos de tipo mesa.");
-            System.out.println("6. Mostrar el precio promedio de los productos cuyo material es madera.");
-            System.out.println("7. Mostrar la información del producto o de los productos "
-                    + "con mayor cantidad de unidades disponibles.");
-            System.out.println("8. Mostrar la fecha y valor pagado para cada una de las compras de un cliente. "
-                    + "Además, debe mostrar el resultado de la sumatoria de los valores pagados por dicho cliente.");
-            System.out.println("9. Graficos:");
-            System.out.println("• La cantidad de productos por material.");
-            System.out.println("• La cantidad de productos por tipo (mesas y sillas).");
-            System.out.println("10. Salir.");
-            opcion = sc.nextInt();
-            
-            switch(opcion){
-                case 1:
-                    String documento, nombre;
-                    System.out.println("Ingrese el numero de documento de identidad:");
-                    documento = sc.next();
-                    System.out.println("Ingrese el nombre completo:");
-                    nombre = sc.next();
-                    System.out.println("Ingrese la fecha de nacimiento (DD/MM/AAAA):");
-                    String fechaTexto = sc.next();
 
-                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-                    LocalDate fecha_n = LocalDate.parse(fechaTexto, formatter);
-                    
-                    Cliente cliente = new Cliente(documento, nombre, fecha_n);
+public class Compra extends JFrame {
 
-                    clientes.put(documento, cliente);
-                    
-                    if (clientes.containsKey(documento)) {
-                        System.out.println("el cliente " + nombre + " ya existe.");
-                    } else {
-                        clientes.put(documento, cliente);
-                        System.out.println("Registro exitoso.");
-                    }
-                    break;
-                case 2:
-                    while(opcion1 != 3){
-                        System.out.println("Ingrese el mueble a registrar:");
-                        System.out.println("1. Mesa.");
-                        System.out.println("2. Silla.");
-                        opcion1 = sc.nextInt();
-                        switch (opcion1){
-                            case 1:
-                                System.out.println("Ingrese el codigo:");
-                                System.out.println("Ingrese la descrip");
-                                
-                        }
-                    }
+    //Estructuras de datos
+    HashMap<String, Cliente> clientes = new HashMap<>();
+    ArrayList<Mueble> productos = new ArrayList<>();
+
+    //Interfaz
+    JMenuBar barra;
+    JMenu menu, graficos;
+    JMenuItem opcion, opcion1, opcion2, opcion3, opcion4, opcion5, opcion6, opcion7, opcion8, opcion9
+    , submenu91, submenu92;
+
+    public Compra(){
+
+        setTitle("Sistema de Compra");
+
+        barra = new JMenuBar();
+        setJMenuBar(barra);
+
+        menu = new JMenu("Menú");
+        barra.add(menu);
+
+        opcion1 = new JMenuItem("Registrar cliente");
+        menu.add(opcion1);
+
+        //Acciones de la opcion 1
+        opcion1.addActionListener(e -> {
+            try {
+                String documento = JOptionPane.showInputDialog("Ingrese el numero de documento de identidad:");
+                
+                String nombre = JOptionPane.showInputDialog("Ingrese el nombre completo:");            
+
+                String fechaTexto = JOptionPane.showInputDialog("Ingrese la fecha de nacimiento (DD/MM/YYYY):");
+
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                LocalDate fecha = LocalDate.parse(fechaTexto, formatter);
+
+                //para ver que no se repita el usuario y agregarlo al hashmap
+                if (clientes.containsKey(documento)) {
+                    JOptionPane.showMessageDialog(null, "El cliente" + nombre + " ya existe");
+                    return;
+                }else{ 
+                    clientes.put(documento, new Cliente(documento, nombre, fecha));
+                    JOptionPane.showMessageDialog(null, "Registro exitoso");
+                }
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(null, "Error en datos");
             }
-            
-            
-        }
-        
-        
+        });
+
+        opcion2 = new JMenuItem("Registrar un producto.");
+        menu.add(opcion2);
+
+        //Acciones de la opcion 2
+        opcion2.addActionListener(e -> {
+            try {
+                int opcion = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el mueble a registrar:\n1. Mesa\n2. Silla"));
+                
+                String codigo = JOptionPane.showInputDialog("Ingrese el codigo:");            
+                String descripcion_producto = JOptionPane.showInputDialog("Ingrese la descripcion del producto:");
+                String material_producto  = JOptionPane.showInputDialog("Ingrese el material:");
+                double precio_procuto = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el precio:"));
+                String color_producto  = JOptionPane.showInputDialog("Ingrese el color:");
+                int cantidad_producto  = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de unidades:"));
+                
+                if (opcion == 1) {
+                    String forma_tapam = JOptionPane.showInputDialog("Ingrese la forma de la tapa:");
+                    double ancho_tapam = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el ancho de la tapa:"));
+                    double largo_tapam = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el largo de la tapa:"));
+                    String material_tapam = JOptionPane.showInputDialog("Ingrese el material de la tapa:");
+
+                    productos.add(new Mesa(codigo, descripcion_producto, material_producto, precio_procuto, color_producto, cantidad_producto, 
+                        forma_tapam, ancho_tapam, largo_tapam, material_tapam));
+
+                }else if(opcion == 2){
+                    String tapizados = JOptionPane.showInputDialog("Ingrese el material de tapizado:");
+                    String colors = JOptionPane.showInputDialog("Ingrese el color del tapizado:");
+                    String densidads = JOptionPane.showInputDialog("Ingrese la densidad de la espuma:");
+
+                    productos.add(new Silla(codigo, descripcion_producto, material_producto, precio_procuto, color_producto, cantidad_producto, 
+                        tapizados, colors, densidads));
+
+                }else{
+                    JOptionPane.showMessageDialog(null, "Opción invalida.");   
+                }
+
+                JOptionPane.showMessageDialog(null, "Producto registrado correctamente.");
+
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(null, "Error en los datos.");
+            }
+        });
+
+        opcion3 = new JMenuItem("Registrar compra.");
+        menu.add(opcion3);
+
+        opcion4 = new JMenuItem("Mostrar los productos de la tienda.");
+        menu.add(opcion4);
+
+        opcion5 = new JMenuItem("Mostrar el porcentaje de mesa.");
+        menu.add(opcion5);
+
+        opcion6 = new JMenuItem("Precio promedio de los productos de madera.");
+        menu.add(opcion6);
+
+        opcion7 = new JMenuItem("Cantidades unitarias de los productos.");
+        menu.add(opcion7);
+
+        opcion8 = new JMenuItem("Fecha y valor pagado para las compras de un cliente.");
+        menu.add(opcion8);
+
+        opcion9 = new JMenuItem("Gráficos.");
+        menu.add(opcion9); 
+        //Submenu de la opcion 9
+        submenu91 = new JMenuItem("Cantidad de prouctos por material.");
+        opcion9.add(submenu91);
+        submenu92 = new JMenuItem("Cantidad por tipo de productos.");
+        opcion9.add(submenu92);
+
+
+        setVisible(true);
+        setSize(400, 300);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        Compra ventana = new Compra();
     }
 }
